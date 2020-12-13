4770f86b150e13ae274b94c1e48f016393a7651e
bitoga <bitoga90@gmail.com>
2020-12-13 23:20:35 +0300

    Configure static files build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const { src, dest, series, parallel, watch} = require('gulp');
-const browsersync = require('browser-sync').create();
-const concat      = require('gulp-concat');
-const cleanCSS    = require('gulp-clean-css');
+const browsersync  = require('browser-sync').create();
+const concat       = require('gulp-concat');
+const cleanCSS     = require('gulp-clean-css');
+const imagemin     = require('gulp-imagemin');
+const newer        = require('gulp-newer');
+const autoprefixer = require('gulp-autoprefixer');
+const sass         = require('gulp-sass');
+
 
 function browserSync() {
     browsersync.init({
@@ -12,24 +17,45 @@ function browserSync() {
 }
 
 function copy() {
-    src('src/index.html')
+   return src('src/index.html')
+        .pipe(concat('index.html'))
         .pipe(dest('dist/'))
+
 }
 
 function styles(){
-    src('src/scss/**/*.scss')
+   return src('src/scss/**/*.scss')
+        .pipe(sass())
         .pipe(concat('index.css'))
+        .pipe(autoprefixer({
+           overrideBrowserslist: ['last 2 versions']
+        }))
         .pipe(cleanCSS())
         .pipe(dest('dist/css/'))
         .pipe(browsersync.stream())
 }
 
+// function reload() {
+//     browsersync.reload();
+// }
+
+function images() {
+    return src('src/images/**/*')
+        .pipe(newer('dist/images/'))
+        .pipe(imagemin())
+        .pipe(dest('dist/images/'))
+}
 
 function startWatch(){
-    watch('src/scss/**/*.scss', styles)
-    watch('src/**/*.html').on('change', browsersync.reload)
+    watch('src/scss/**/*.scss', styles);/*series(styles, reload)*/
+    watch(['src/images/**/*', 'dist/images/**/*'], images);
+    watch('src/**/*.html', copy);/*series(copy, reload)*/
+    watch('dist/**/*.html').on('change', browsersync.reload);
+
 }
 
 exports.browsersync = browserSync;
 exports.styles      = styles;
-exports.default     = parallel(copy, styles, browserSync, startWatch);
+exports.images      = images;
+exports.copy        = copy;
+exports.default     = parallel(copy, styles, images, browserSync, startWatch);
